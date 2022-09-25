1cad2082e5a808df3e05b0ab429a7d55a50916c6
Jean Paul Bernhardt <beralb@tutanota.com>
2022-09-25 16:54:13 -0300

    conclusao req 04 correcao

--- desafio4.sql
+++ desafio4.sql
@@ -1,14 +1,16 @@
 SELECT 
-	u.usuario AS usuario,
-    COUNT(h.data_reproducao)
-    
-FROM 
+	MIN(u.usuario) AS usuario,
+    IF (MAX(h.data_reproducao) >= '2021-01-01', 'Usuário ativo', 'Usuário inativo') AS status_usuario
+FROM
+	SpotifyClone.usuarios AS u	
+INNER JOIN	
 	SpotifyClone.historico_de_reproducoes AS h
-INNER JOIN
-	SpotifyClone.usuarios AS u
 ON 
 	u.usuario_id = h.usuarios_usuario_id
 
 GROUP BY
-	u.usuario,
-	h.data_reproducao;
+	u.usuario
+
+ORDER BY
+		u.usuario;
+        
