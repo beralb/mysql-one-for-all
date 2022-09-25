ef67be03a801071921b019f43e05be7966b259c1
Jean Paul Bernhardt <beralb@tutanota.com>
2022-09-25 14:17:59 -0300

    conclusao req 10

---
+++ desafio10.sql
@@ -0,0 +1,25 @@
+SELECT 
+	c.cancao,
+    COUNT(h.data_reproducao) AS reproducoes
+FROM
+	SpotifyClone.cancoes as c
+    
+INNER JOIN
+	SpotifyClone.historico_de_reproducoes as h
+ON 
+	h.cancoes_cancao_id = c.cancao_id
+    
+INNER JOIN
+	SpotifyClone.usuarios AS u
+ON
+	h.usuarios_usuario_id = u.usuario_id
+    
+WHERE
+	u.planos_plano_id = 1 OR u.planos_plano_id = 4
+    
+GROUP BY
+c.cancao
+
+ORDER BY
+	c.cancao;
+    
